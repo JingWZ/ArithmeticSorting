7edef2ed7317fb95f7caa30712191b6c1176f5a0
JingWZ <benlaijw@benlaijwdeimac.lan>
2015-11-30 14:52:01 +0800

    Initial Commit

---
+++ ArithmeticSorting/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArithmeticSorting
+//
+//  Created by benlaijw on 15/11/30.
+//  Copyright © 2015年 com.jing. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
