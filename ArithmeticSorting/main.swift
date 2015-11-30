8cecb81a3eafc194407df85f3d535a6ca45fa8f0
JingWZ <benlaijw@benlaijwdeiMac.lan>
2015-11-30 17:42:01 +0800

    bubble sorting

--- ArithmeticSorting/main.swift
+++ ArithmeticSorting/main.swift
@@ -10,3 +10,50 @@ import Foundation
 
 print("Hello, World!")
 
+func randomList(count: Int) -> [Int] {
+    
+    let max = 100
+    if count > max {
+        assertionFailure()
+    }
+    
+    var randomList = [Int]()
+    for _ in 1...count {
+        let random = Int(arc4random()) % max
+        randomList.append(random)
+    }
+    
+    print("randomList:\(randomList)")
+    
+    return randomList
+}
+
+let originalList = randomList(10)
+
+func bubbleSorting() {
+    
+    var sortedList = originalList
+    var count = sortedList.count // for skipping sorted elements
+    var swapped = false // for not sorting while no swapping in previous turn
+    
+    repeat {
+        swapped = false
+        for var index = 0; index < count - 1; index++ {
+            var a = sortedList[index]
+            var b = sortedList[index + 1]
+            
+            if a > b {
+                swap(&a, &b)
+                sortedList[index] = a
+                sortedList[index + 1] = b
+                swapped = true
+            }
+        }
+        count--
+    } while swapped
+    
+    print("sortedList:\(sortedList)")
+}
+
+bubbleSorting()
+
